c84530de64ea8acd02e71c894cb4b344440f9ea6
nufarlevy2 <levynufar@gmail.com>
2018-04-24 13:19:24 +0300

    adding cantching SIGPIPE to sym_count

--- ex2/sym_count.c
+++ ex2/sym_count.c
@@ -15,10 +15,13 @@
 int occurance = 0;
 const char *charSearchQuery;
 int openedFile;
+char *sharedFileArray;
+size_t sizeOfFile;
+int sharedFile;
 
 //Decleration for Handler
 void handler(int sig);
-void exitWithError(const char *msg, int openedFile, const char *sharedFile);
+void exitWithError(const char *msg, int openedFile, int sharedFile);
 
 int main(int argc, char* argv[]) {
 	//Setting a struct to handle the SIGSTOP and SIGTERM signals
@@ -33,24 +36,21 @@ int main(int argc, char* argv[]) {
         const char *filePath = argv[1];
         charSearchQuery = argv[2];
 	off_t offset = 0;
-	size_t sizeOfFile;
-	char *sharedFileArray;
 	char nameOfFifo[12] = "/tmp/abcdea";
-	int sharedFile;
 	char finalString[60] = {0};
 	//Checks on user input
 	if (argc != 3) {
-		exitWithError("\nwrong use in function! please insert\n./a.out <filePath> <charToLookFor>\n", -1, "notRelevant");
+		exitWithError("\nwrong use in function! please insert\n./a.out <filePath> <charToLookFor>\n", -1, -1);
 	}
 	if (access(filePath,R_OK) != 0) {
 		
-		exitWithError("\nFile does not exist or you do not have the read permissions\n", -1, "notRelevant");
+		exitWithError("\nFile does not exist or you do not have the read permissions\n", -1, -1);
 	}
 	else {
 		// Opening the file and loading it to mmap
 		openedFile = open(filePath, O_RDWR);
 		if (openedFile < 0) {
-			exitWithError("\nFailed opening the file\n", -1, "notRelevant");
+			exitWithError("\nFailed opening the file\n", -1, -1);
 		}
 		lseek(openedFile, (size_t)0, SEEK_CUR);
 		sizeOfFile = lseek(openedFile, (size_t)0, SEEK_END);
@@ -58,13 +58,13 @@ int main(int argc, char* argv[]) {
 		sharedFileArray = (char *)mmap(NULL, sizeOfFile, PROT_READ, MAP_SHARED, openedFile, 0);
 	}
 	if ( strlen(charSearchQuery) != 1) {
-		exitWithError("\ninvalid char input - the only usage is to search for one character\n", openedFile, filePath);
+		exitWithError("\ninvalid char input - the only usage is to search for one character\n", openedFile, -1);
 	}
 	//Reading the data from mmap and counting the symbol
 	nameOfFifo[10] = charSearchQuery[0];
 	sharedFile = open(nameOfFifo, O_WRONLY);
 	if (sharedFile == -1) {
-		exitWithError("Failed to open fifo\n", openedFile,filePath);
+		exitWithError("Failed to open fifo\n", openedFile,sharedFile);
 	}
 	while ( offset < sizeOfFile) {
 		if (charSearchQuery[0] == sharedFileArray[offset]) {
@@ -75,23 +75,43 @@ int main(int argc, char* argv[]) {
 	close(openedFile);
 	snprintf(finalString, sizeof(finalString), "Process %d finished. Symbol %c. Instances %d.\n", getpid(), charSearchQuery[0], occurance);
 	if (write(sharedFile, finalString, sizeof(finalString)) == -1){
-		exitWithError("Faled to write to fifo\n", openedFile, filePath);
+		exitWithError("Faled to write to fifo\n", openedFile, sharedFile);
+	}
+	if (munmap(sharedFileArray, sizeOfFile) == -1) {
+		exitWithError("Faled munmap!\n", openedFile, sharedFile);
+	}
+	if (openedFile >= 0) {
+	 	close(openedFile);
+	}
+	if (sharedFile >= 0) {
+		close(sharedFile);
 	}
 	exit(EXIT_SUCCESS);
 }
 
 //Handler for the SIGTERM and SIGCONT signals
 void handler(int sig){
-       if (sig == SIGTERM) {
-	       printf("Process %d finishes. Symbol %c. Instances %d.\n", getpid(), charSearchQuery[0], occurance);
+       if (sig == SIGPIPE) {
+	       printf("SIGPIPE for process %d. Symbol %c. Counter %d. Leaving.", getpid(), charSearchQuery[0], occurance);
+	       if (openedFile >= 0) {
+      		       close(openedFile);
+		       munmap(sharedFileArray, sizeOfFile);
+	       }
+	       if (sharedFile >=0) {
+		       close(sharedFile);
+	       }
 	       exit(EXIT_SUCCESS);
-       }	       
+       } 
 }
 
-void exitWithError(const char *msg, int filed, const char *sharedFile) {
+void exitWithError(const char *msg, int openedFile, int sharedFile) {
 	perror(msg);
-	if (filed >= 0) {
-		close(filed);
+	if (openedFile >= 0) {
+		close(openedFile);
+		munmap(sharedFileArray, sizeOfFile);
+	}
+	if (sharedFile >=0) {
+		close(sharedFile);
 	}
 	exit(EXIT_FAILURE);
 }
