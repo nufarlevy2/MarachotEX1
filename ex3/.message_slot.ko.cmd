aae45ca1fd01260b90ad1892a8a8aef64b0dd97a
nufarlevy2 <levynufar@gmail.com>
2018-05-19 16:23:25 +0300

    adding ex4 folder

---
+++ ex3/.message_slot.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/nufar/exresices/exerecisesGit/ex3/message_slot.ko := ld -r -m elf_i386 -T ./scripts/module-common.lds --build-id  -o /home/nufar/exresices/exerecisesGit/ex3/message_slot.ko /home/nufar/exresices/exerecisesGit/ex3/message_slot.o /home/nufar/exresices/exerecisesGit/ex3/message_slot.mod.o ;  true
---
+++ ex3/.tmp_versions/message_slot.mod
@@ -0,0 +1,3 @@
+/home/nufar/exresices/exerecisesGit/ex3/message_slot.ko
+/home/nufar/exresices/exerecisesGit/ex3/message_slot.o
+
