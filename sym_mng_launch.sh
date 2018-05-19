02eb6dd02fa33821bd75a4ef93bd662f9f15d735
nufarlevy2 <levynufar@gmail.com>
2018-05-19 17:08:04 +0300

    Delete sym_mng_launch.sh

--- sym_mng_launch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-fullPathToDataFile=$PATH_TO_DATA/$DATA_FILE #Concating 2 strings to the full path of the data file
-chmod u+rwx $fullPathToDataFile #Giving all permissions to the user
-chmod go-rwx $fullPathToDataFile #Reducing all permissions to group and other users
-$FULL_EXE_NAME $PATH_TO_DATA/$DATA_FILE $PATTERN $BOUND & #Executing the prog with all parameters in the background!
-
