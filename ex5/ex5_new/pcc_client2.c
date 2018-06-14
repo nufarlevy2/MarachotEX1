5fe35e8c5e6a88f30cdc19bac4b5590676316d68
nufarlevy2 <levynufar@gmail.com>
2018-06-14 18:19:59 +0300

    Update pcc_client2.c

--- ex5/ex5_new/pcc_client2.c
+++ ex5/ex5_new/pcc_client2.c
@@ -10,103 +10,68 @@
 #include <netdb.h>
 #include <sys/fcntl.h>
 #include <sys/wait.h>
-#include <sys/types.h>
-#include <string.h>
 
+#define MAX_UNSIGNED_SHORT 65535
 
 in_addr_t hostNameToIp(char* serverHost);
+
+int getConnection(unsigned short port,in_addr_t serverIP, unsigned int length);
+
+char* getRandomChars(unsigned int length);
+
 unsigned int sendToConnection(char* bufferToSend,int sockfd,unsigned int length);
-void convertToChars(unsigned int unsignedIntInput, char buffer[]);
-void sendRandomCharsToServer(char* bufferToSend,int sockfd,unsigned int length);
-unsigned int convertToUnsignedInt(char* buffer);
+
 void sendLengthToServer(int sockfd,unsigned int length);
 
-int main(int argc, char *argv[]) {
-	
-	int rv;
-	char* serverHost;
-	unsigned short serverPort;
-	unsigned int length;
-	in_addr_t ip;
-	struct sockaddr_in serverAddr;
-	char* buffer;
-	int randomFile;
-	ssize_t dataRead;
-	ssize_t currentRead;
-	ssize_t anotherRead;
-	unsigned int numOfPrintableCharacters;
-	int socketNum;
+void sendRandomCharsToServer(char* bufferToSend,int sockfd,unsigned int length);
 
-	// initial checkups
-	if (argc < 4) {
-	    	printf("Invalid usage of this function please use:\n./prog <server_host> <server_port> <length>");
-		exit(EXIT_FAILURE);
-    	}
-    	serverHost = argv[1];
-	serverPort = (unsigned short)strtoul(argv[2], NULL, 0);
-	if (serverPort == 0) {
-		perror("Not a valid port!");    
-		exit(EXIT_FAILURE);
-	}    
-	length = (unsigned int)strtoul(argv[3], NULL, 0);    
-	if (length == 0) {
-		perror("Not a valid length!");
-		exit(EXIT_FAILURE);    
-	}
-    	printf("server: %s, port: %s, length: %s\n",argv[1], argv[2], argv[3]);
-	// initilizing all host parameters
-	ip = hostNameToIp(serverHost);
-	socketNum = socket(AF_INET, SOCK_STREAM, 0);
-	if(socketNum < 0) {
-		perror("Error in creating socket");
-		exit(EXIT_FAILURE);
-	}
-	serverAddr.sin_addr.s_addr = ip;
-	serverAddr.sin_port = htons(serverPort);
-	serverAddr.sin_family = AF_INET;
-	memset(&serverAddr, 0, sizeof(serverAddr));
-	printf("Before connecting to server\n");	
-	// conecting to server
-	rv = connect(socketNum, (struct sockaddr*) &serverAddr, sizeof(serverAddr));
-	if(rv < 0) {
-		perror("Error in Connecting to server");
-		exit(EXIT_FAILURE);
-	}
-	
-	// getting the tandom characters to buffer
-	buffer = (char*)malloc(sizeof(char)*length);
-	if (buffer == NULL) {
-		perror("Error in mallocing buffer");
-		exit(EXIT_FAILURE);
-	}
-	memset(buffer,0,length);
-	randomFile = open("/dev/urandom", O_RDONLY);
-	if (randomFile < 0) {
-		perror("Could not open urandom!");
-		exit(EXIT_FAILURE); 
-	}
-	dataRead = read(randomFile, buffer, length);
-	if (dataRead < 0) {
-		perror("Could not read from urandom");
-		exit(EXIT_FAILURE);
-	}
-	while (dataRead < length) {
-		currentRead = dataRead;
-		anotherRead = read(randomFile, currentRead+buffer, length-currentRead);
-		if (anotherRead < 0) {
-			perror("Could not read from urandom");
-			exit(EXIT_FAILURE);
-		}
-		dataRead = dataRead + anotherRead;
-	}
-	printf("After buffer read\n");
-	//sending data to the server
-    	numOfPrintableCharacters = sendToConnection(buffer, socketNum, length);
-	printf("After sending data to server\n");
-    	printf("# of printable characters: %u\n", numOfPrintableCharacters);
-    	close(socketNum);
-    	free(buffer);
-    	exit(EXIT_SUCCESS);
+void unsignedIntToChars(unsigned int num, char buffer[]);
+
+unsigned int charsToUnsignedInt(char* buffer);
+
+
+int main(int argc, char *argv[]) {
+    if (argc < 4)
+    {
+        printf("Error1: Not enough parameters");
+        exit(-1);
+    }
+
+    int sockfd;
+    char* serverHost;
+    in_addr_t serverIP;
+    unsigned short port;
+    unsigned int length;
+    unsigned int numOfPrintable;
+    char* bufferToSend;
+
+    serverHost = argv[1];
+    //may need to convert the host name to an IP address first
+    serverIP = hostNameToIp(serverHost);
+    port = (unsigned short)strtoul(argv[2], NULL, 0);
+    if ((int)port == 0 || (int)port > MAX_UNSIGNED_SHORT)
+    {
+        perror("ERROR2: bad port");
+        exit(-1);
+    }
+    length = (unsigned int)strtoul(argv[3], NULL, 0);
+    if ((int)length == 0)
+    {
+        perror("ERROR3: bad char num");
+        exit(-1);
+    }
+
+    //1. Create a TCP connection to the specified server port on the specified server host.
+    sockfd = getConnection(port,serverIP,length);
+    //2. Open /dev/urandom for reading.
+    bufferToSend = getRandomChars(length);
+    //3. Transfer the specified length bytes from /dev/urandom to the server over the TCP connection.
+    numOfPrintable = sendToConnection(bufferToSend, sockfd, length);
+    //4. Obtain the count of printable characters computed by the server, C (an unsigned int), and print it
+    printf("# of printable characters: %u\n", numOfPrintable);
+    close(sockfd);
+    free(bufferToSend);
+    exit(0);
 }
 
 in_addr_t hostNameToIp(char* serverHost) {
@@ -139,6 +104,67 @@ in_addr_t hostNameToIp(char* serverHost) {
     	ip = serverInAddr.s_addr;
     	return ip;
 }
+
+int getConnection(unsigned short port, in_addr_t serverIP, unsigned int length)
+{
+    int sockfd = -1;
+    struct sockaddr_in serv_addr;
+
+    if( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("Error6 : Could not create socket.");
+        exit(-1);
+    }
+
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port); // Note: htons for endiannes
+    serv_addr.sin_addr.s_addr = serverIP;
+
+    if(0 > connect(sockfd, (struct sockaddr*) &serv_addr, sizeof(serv_addr)))
+    {
+        perror("Error7 : Connect Failed.");
+        exit(-1);
+    }
+    return sockfd;
+}
+
+char* getRandomChars(unsigned int length)
+{
+    char* bufferToSend;
+    ssize_t result;
+    ssize_t lastResult;
+    bufferToSend = (char*)malloc(sizeof(char)*length);
+
+    if (bufferToSend == NULL)
+    {
+        perror("ERROR8");
+        exit(-1);
+    }
+    memset(bufferToSend,0,sizeof(bufferToSend));
+
+    int randomData = open("/dev/urandom", O_RDONLY);
+    if (randomData < 0)
+    {
+        perror("ERROR9");
+        exit(-1);
+    }
+
+    result = read(randomData, bufferToSend, length);
+    while ( result < length)
+    {
+        if (result < 0)
+        {
+            perror("ERROR10");
+            exit(-1);
+        }
+        lastResult = result;
+        result += read(randomData, bufferToSend+lastResult, sizeof(bufferToSend)-lastResult);
+    }
+
+    return bufferToSend;
+}
+
 unsigned int sendToConnection(char* bufferToSend,int sockfd,unsigned int length)
 {
     unsigned int numOfPrintables;
@@ -162,7 +188,7 @@ unsigned int sendToConnection(char* bufferToSend,int sockfd,unsigned int length)
         bytes_read += read(sockfd, numOfPrintablesBuffer+lastRead, sizeof(unsigned int)-lastRead);
     }
 
-    numOfPrintables = convertToUnsignedInt(numOfPrintablesBuffer);
+    numOfPrintables = charsToUnsignedInt(numOfPrintablesBuffer);
     return numOfPrintables;
 }
 
@@ -174,7 +200,7 @@ void sendLengthToServer(int sockfd,unsigned int length)
     int notWritten;
     int lastWrite;
 
-    convertToChars(length,lengthBuffer);
+    unsignedIntToChars(length,lengthBuffer);
     notWritten = (int)sizeof(unsigned int); // how much we have left to write
     totalSent = 0; // how much we've written so far
     lastWrite = -1; // how much we've written in last write() call */
@@ -217,89 +243,22 @@ void sendRandomCharsToServer(char* bufferToSend,int sockfd,unsigned int length)
         notWritten -= lastWrite;
     }
 }
-/*
-unsigned int sendingDataToServer(char* buffer,int socketNum,unsigned int length) {
-	char numOfPrintablesInChars[4];
-    	unsigned int numOfPrintableCharacters;
-	ssize_t numOfBytesReadWrote;
-	ssize_t currentReadWrite;
-	ssize_t anotherReadWrite;	
-	char lengthInChars[sizeof(unsigned int)];
-	int lengthToReadWrite;
-	printf("Sending length\n");
-	// sending length to server
-	lengthToReadWrite = (int)sizeof(unsigned int);
-	convertToChars(length,lengthInChars);
-	printf("before write %c%c%c%c\n",lengthInChars[0],lengthInChars[1],lengthInChars[2],lengthInChars[3]);
-	printf("writing socketNum %d, lengthToReadWrite: %d\n",socketNum,lengthToReadWrite);
-	numOfBytesReadWrote = (int) write(socketNum, lengthInChars+0, (size_t)lengthToReadWrite);
-	if (numOfBytesReadWrote < 0) {
-		perror("Could not write length to server");
-		exit(EXIT_FAILURE);                         
-	}
-	printf("after written the first time\n");
-   	while(numOfBytesReadWrote < lengthToReadWrite) {
-		currentReadWrite = numOfBytesReadWrote;
-		anotherReadWrite = (int)write(socketNum, lengthInChars+currentReadWrite, (size_t)lengthToReadWrite-currentReadWrite);
-		if (anotherReadWrite < 0) {
-			perror("Could not write length to server");
-			exit(EXIT_FAILURE);
-		}
-		numOfBytesReadWrote = numOfBytesReadWrote + anotherReadWrite;
-	}
-	printf("Sending data\n");
-	// sending data to server
-	numOfBytesReadWrote = (int)write(socketNum, buffer, (size_t)length);
-	if (numOfBytesReadWrote < 0) {
-		perror("Could not write data to server");
-		exit(EXIT_FAILURE);
-	}
-	while( numOfBytesReadWrote < length ) {
-		currentReadWrite = numOfBytesReadWrote;
-		anotherReadWrite = (int)write(socketNum, buffer+currentReadWrite, (size_t)length-currentReadWrite);
-		if (anotherReadWrite < 0) {
-			perror("Could not write length to server");
-			exit(EXIT_FAILURE);
-		}
-		numOfBytesReadWrote = numOfBytesReadWrote + anotherReadWrite;
-	}	
-	printf("recieving response\n");
-	//recieving response from server
-    	memset(numOfPrintablesInChars, 0, sizeof(numOfPrintablesInChars));
-    	numOfBytesReadWrote = read(socketNum, numOfPrintablesInChars, (size_t)lengthToReadWrite);
-	if (numOfBytesReadWrote < 0) {
-		perror("Could not read from server");
-		exit(EXIT_FAILURE);
-	}
-    	while (numOfBytesReadWrote < lengthToReadWrite) { 
-		currentReadWrite = numOfBytesReadWrote;
-		anotherReadWrite = read(socketNum, numOfPrintablesInChars+currentReadWrite, (size_t)lengthToReadWrite-currentReadWrite);
-		if (anotherReadWrite < 0) {
-			perror("Could not read from server");
-			exit(EXIT_FAILURE);
-		}
-		numOfBytesReadWrote = numOfBytesReadWrote + anotherReadWrite;
-	}
-    	numOfPrintableCharacters = convertToUnsignedInt(numOfPrintablesInChars);
-	printf("returning protible letters\n");
-    	return numOfPrintableCharacters;
-}
-*/
-unsigned int convertToUnsignedInt(char* buffer)
+
+unsigned int charsToUnsignedInt(char* buffer)
 {
-    unsigned int result = (unsigned int)(buffer[0]) << 24;
-    result |= (unsigned int)(buffer[1]) << 16;
-    result |= (unsigned int)(buffer[2]) << 8;
-    result |= (unsigned int)(buffer[3]);
-    return result;
+    unsigned int N;
+    N = (unsigned int)(buffer[0]) << 24;
+    N |= (unsigned int)(buffer[1]) << 16;
+    N |= (unsigned int)(buffer[2]) << 8;
+    N |= (unsigned int)(buffer[3]);
+    return N;
 }
 
 
-void convertToChars(unsigned int unsignedIntInput, char buffer[]) {
-//	printf("converting to chars unsigned int: %d\n",(int)unsignedIntInput);
-    	buffer[0] = (unsigned char)((unsignedIntInput >> 24 & 255));
-    	buffer[1] = (unsigned char)((unsignedIntInput >> 16 & 255));
-    	buffer[2] = (unsigned char)((unsignedIntInput >> 8 & 255));
-    	buffer[3] = (unsigned char)(unsignedIntInput & 255);
-//	printf("buffer is %c%c%c%c\n",buffer[0],buffer[1],buffer[2],buffer[3]);
+void unsignedIntToChars(unsigned int num, char buffer[])
+{
+    buffer[0] = (char)((num >> 24) & 0xFF);
+    buffer[1] = (char)((num >> 16) & 0xFF);
+    buffer[2] = (char)((num >> 8) & 0xFF);
+    buffer[3] = (char)(num & 0xFF);
 }
